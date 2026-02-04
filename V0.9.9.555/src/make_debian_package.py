c77869535e1bbac606c095e3efd67068b24fd8c9
molly9919 <molly9@zohomail.eu>
2026-02-05 00:14:17 +0100

    Drop unused python3 crypto/pil dependencies

--- V0.9.9.555/src/make_debian_package.py
+++ V0.9.9.555/src/make_debian_package.py
@@ -15,7 +15,7 @@ Section: internet
 Priority: optional
 Architecture: all
 Essential: no
-Depends: tor, python3 (>= 3.4), python3-wxgtk4.0 | python3-wxgtk3.0 | python3-wxgtk, python3-crypto, python3-pil
+Depends: tor, python3 (>= 3.4), python3-wxgtk4.0 | python3-wxgtk3.0 | python3-wxgtk
 Maintainer: Haxxie       <haxxie@safe-mail.net>
 Provides: tor.chat
 Description: Secure Instant Messenger for Tor
